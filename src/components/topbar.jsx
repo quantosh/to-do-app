351f601e492ce250bcf86ca34b84d4bb678425d9
Ismael Barea Insua <80041279+quantosh@users.noreply.github.com>
2023-02-06 12:55:18 +0100

    Added a link in the topbar to Trello
    Public kanban board

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -26,6 +26,9 @@ function Topbar () {
             <a href='#responsive-header' class='block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white'>
               Tasks
             </a>
+            <a href='https://trello.com/b/K7IETmRy/to-do-app' class='block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4'>
+              Trello
+            </a>
           </div>
           <div>
             <a href='#' class='inline-block text-sm px-4 py-2 leading-none border rounded text-black border-white hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0 mr-1'>Login</a>
